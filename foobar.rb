e393c68a107e58b3aad63952a2ff6dc82a173e53
Aaron Xu <aaron_rg_xu@hotmail.com>
2016-09-20 02:16:37 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|a| a = a.to_i + 2}
+    a.uniq!
+    a.delete_if {|a| a > 10 || a % 2 != 0}
+    sum = 0
+    a.each {|a| sum += a}
+    sum
   end
 end
 
